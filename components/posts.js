a46287edf0b258de10736a534ee123634d5cc924
given-woo <2210220@bsis.hs.kr>
2022-10-20 22:56:21 +0900

    jaegui2

--- components/posts.js
+++ components/posts.js
@@ -5,6 +5,7 @@ import Post from './post'
 export default function Posts() {
     return (
         <div className={styles.posts}>
+            <Post imageUrl="https://media2.giphy.com/media/IpeYSEZshTefe/giphy.gif?cid=ecf05e4778m6kru4sp8blcgdaxbodrs2zdh1am56yjvc4ypn&rid=giphy.gif&ct=g" title="똥멍청이도 이해하는 재귀함수" text="아직도 재귀함수가 뭔지 몰라? 이해를 못한 똥뭉탱이들을 위한 재귀함수 자습서! 재귀함수의 본질을 알아보자!!" src="/jaegui2" />
             <Post imageUrl="https://media2.giphy.com/media/jAe22Ec5iICCk/giphy.gif?cid=ecf05e47k550qwlh25j4nt86230jl7q7hw2zi2vo36px1qka&rid=giphy.gif&ct=g" title="For문 마스터 되기" text="아직도 for문을 이해 못한 사람들을 위한 for문 자습서! 10분만에 for문을 이해해보자!" src="/for" />
             <Post imageUrl="https://media4.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif?cid=ecf05e47jmerhdcnywqdse7oy94srw2zltkw49iu0htkhx8i&rid=giphy.gif&ct=g" title="[코드업] 문제 풀이 노트" text="코드업 문제집 풀면서 놓쳤던 문제 정리! 하루 5문제씩 업데이트 됩니다." src="/codeup" />
             <Post imageUrl="https://media0.giphy.com/media/S8TsC0rUwf9xGA3Hx7/giphy.gif?cid=ecf05e47errwly7lmyl3tcdmj09sf13xzhctkz9j2djqednk&rid=giphy.gif&ct=g" title="탐색기반설계 vs 관계기반설계" text="탐색기반설계는 뭐지? 관계기반설계는 또 뭘까? 알고리즘 설계 방법을 배워서 야무지게 알고리즘을 짜보자!" src="/search_algo" />
---
+++ pages/jaegui2.js
@@ -0,0 +1,39 @@
+import styles from '../styles/view.module.css'
+
+import Page from './jaegui2_md.mdx';
+import { Heading1, Heading2, Heading3, Heading4, Text, Divider, List_ul, List_li, Quote, a_tag, Codeblock, Code, Img, List_ol } from '../components/view';
+import Head from 'next/head';
+
+const components = {
+    h1: Heading1,
+    h2: Heading2,
+    h3: Heading3,
+    h4: Heading4,
+    p: Text,
+    hr: Divider,
+    ol: List_ol,
+    ul: List_ul,
+    li: List_li,
+    blockquote: Quote,
+    pre: Codeblock,
+    a: a_tag,
+    code: Code,
+    img: Img
+}
+
+export default function page1() {
+    return(
+        <div class={styles.wrap}>
+            <Head>
+                <link href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css" rel="stylesheet"/>
+                <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack.css"></link>
+                <meta property="og:description" content="아직도 재귀함수가 뭔지 몰라? 이해를 못한 똥뭉탱이들을 위한 재귀함수 자습서! 재귀함수의 본질을 알아보자!!"></meta>
+                <title>똥멍청이도 이해하는 재귀함수</title>
+            </Head>
+            <div className={styles.content}>
+                <h1 className={styles.title}>똥멍청이도 이해하는 재귀함수</h1>
+                <Page components={components} />
+            </div>
+        </div>
+    );
+}
